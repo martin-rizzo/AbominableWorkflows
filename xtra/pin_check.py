6c6620dfa271c5c6b8044068a0d6b4d3324d5dd1
Martin Rizzo <martinrizzo@gmail.com>
2024-09-03 18:48:18 -0300

    fix: Expand pin_check.py to check node attributes
    This commit expands the `pin_check.py` script to also check
    the `pos` and `size` attributes of each node in a workflow. This is
    to prevent a bug of ComfyUI that may modify these attributes unexpectedly.
    
    The script now checks if all nodes are pinned and if the `pos` and `size`
    attributes are consistent with the expected values. This helps ensure workflow
    stability and prevent unexpected layout changes.

--- xtra/pin_check.py
+++ xtra/pin_check.py
@@ -1,32 +1,32 @@
 import argparse
 import json
 
+RED = "\033[91m"
+ENDC = "\033[0m"
 
-def get_unpinned_nodes(filename):
-    """Extracts unpinned nodes from a workflow JSON file.
+def get_unpinned_nodes(workflow):
+    """Extracts unpinned nodes from a workflow
 
-    This function reads a workflow JSON file and extracts all nodes that are not
-    pinned. It parses the node's title, position (x, y coordinates), and pinned
-    flag.
+    This function extracts all nodes that are not pinned.
 
     Args:
-        filename (str): The path to the workflow JSON file.
+        workflow (dict): A dictionary representing a workflow.
 
     Returns:
-        list: A list of unpinned nodes, each represented as a namedtuple with
-              'name', 'x', and 'y' attributes.
+        list: A list of unpinned nodes, each represented as a namedtuple
+              with 'name', 'x', and 'y' attributes.
     """
     unpinned_nodes = []
 
-    with open(filename, 'r') as f:
-        workflow = json.load(f)
+    nodes = workflow.get('nodes')
+    if not nodes:
+        return []
 
-    for workflow_node in workflow['nodes']:
-        title = workflow_node.get('title', workflow_node.get('type', '?'))
-
-        flags = workflow_node.get('flags')
+    for node in nodes:
+        title       = node.get('title', node.get('type', '?'))
+        flags       = node.get('flags')
         pinned_flag = flags and flags.get('pinned')
-        position = workflow_node.get('pos')
+        position    = node.get('pos')
 
         # extract x and y coordinates
         x, y = 0, 0
@@ -38,10 +38,61 @@ def get_unpinned_nodes(filename):
 
         # append unpinned nodes
         if not pinned_flag:
-            node = type('Node', (), {'name': title, 'x': x, 'y': y})
-            unpinned_nodes.append(node)
+            unpinned_node = type('Node', (), {'name': title, 'x': x, 'y': y})
+            unpinned_nodes.append(unpinned_node)
 
     return unpinned_nodes
+
+def is_two_element_array_like(data):
+    """Checks if the input data is a two-element array-like structure
+
+    This function checks if the input data is either a list or a dictionary.
+    If it's a list, it verifies if it has exactly two elements.
+    If it's a dictionary, it checks if it contains keys '0' and '1'.
+
+    Args:
+        data: The input data to be checked.
+
+    Returns:
+        True if the input data is a two-element array-like structure
+    """
+    if isinstance(data, list):
+        return len(data) == 2
+    elif isinstance(data, dict):
+        return len(data) == 2 and '0' in data and '1' in data
+    else:
+        return False
+
+def check_node_dimensions(workflow):
+    """Checks if the 'pos' and 'size' node attr are valid two element array-like structures
+
+    This function iterates through the 'nodes' in a given workflow and checks
+    if the 'pos' and 'size' attributes of each node are valid two element
+    array-like structures.
+
+    Args:
+        workflow: The workflow dictionary containing the 'nodes' list.
+
+    Returns:
+        A tuple containing the number of nodes with invalid 'pos' and 'size'
+        attributes, respectively.
+    """
+    pos_bug_count  = 0
+    size_bug_count = 0
+
+    nodes = workflow.get('nodes')
+    if not nodes:
+        return None
+
+    for node in nodes:
+        pos  = node.get('pos')
+        size = node.get('size')
+        if pos and not is_two_element_array_like(pos):
+            pos_bug_count += 1
+        if size and not is_two_element_array_like(size):
+            size_bug_count += 1
+
+    return pos_bug_count, size_bug_count
 
 
 #===========================================================================#
@@ -50,7 +101,7 @@ def get_unpinned_nodes(filename):
 
 def main():
     parser = argparse.ArgumentParser(
-        description = "Checks if ComfyUI workflows have all their nodes pinned."
+        description = "Analyzes ComfyUI workflow files to check for issues."
         )
     parser.add_argument("workflow_file", nargs="+", help="ComfyUI workflow file(s) (.json) to analyze.")
     parser.add_argument("--verbose", action="store_true", help="Show additional information about unpinned nodes.")
@@ -58,12 +109,24 @@ def main():
     args = parser.parse_args()
 
     for filename in args.workflow_file:
-        unpinned_nodes = get_unpinned_nodes(filename)
+
+        print()
+        print(filename)
+        with open(filename, 'r') as f:
+            workflow = json.load(f)
+
+        pos_bug_count, size_bug_count = check_node_dimensions(workflow);
+        if pos_bug_count > 0:
+            print(f"{RED}  - Potential issues with 'pos' attribute : {pos_bug_count}{ENDC}")
+        if size_bug_count > 0:
+            print(f"{RED}  - Potential issues with 'size' attribute: {size_bug_count}{ENDC}")
+
+        unpinned_nodes = get_unpinned_nodes(workflow)
         if unpinned_nodes:
-            print(f"File: {filename}")
-            print("Unpinned nodes:")
+            print(f"{RED}  - Found {len(unpinned_nodes)} unpinned nodes:{ENDC}")
             for node in unpinned_nodes:
-                print(f"  - {node.name}  ({node.x}, {node.y})")
+                #print(f"       {node.name}  ({node.x}, {node.y})")
+                print(f"       ({node.x:>4},{node.y:>4}) {node.name}")
         else:
             print(f"All nodes in {filename} are pinned.")
 
