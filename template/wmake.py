d7f6814789b999ce18e10e5986b0bc447fd217bc
Martin Rizzo <martinrizzo@gmail.com>
2024-09-13 12:16:53 -0300

    feat: Implement wmake.py to generate workflows from templates
    This commit implements the `wmake.py` command, which generates workflows
    based on a template and a configuration file. The command now generates
    a workflow using a similar syntax to `make`, but the generated workflow
    is currently identical to the template.
    
    The next step is to apply the configurations correctly.

--- template/wmake.py
+++ template/wmake.py
@@ -1,6 +1,6 @@
 """
   File    : wmake.py
-  Brief   : Creates workflows from a template and a configuration file
+  Brief   : Creates workflows from a template and a config file
   Author  : Martin Rizzo | <martinrizzo@gmail.com>
   Date    : Sep 12, 2024
   Repo    : https://github.com/martin-rizzo/T5ExtendedEncoder
@@ -33,21 +33,33 @@ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 """
 import os
 import sys
+import json
 import argparse
 
-# ANSI escape codes for colored terminal output.
-CYAN   = '\033[96m'
-YELLOW = '\033[93m'
+# ANSI escape codes for colored terminal output
 RED    = '\033[91m'
+GREEN  = '\033[92m'
+YELLOW = '\033[93m'
+CYAN   = '\033[96m'
 DEFAULT_COLOR = '\033[0m'
 
-# Get the directory of the current Python script.
+# Get the directory of the current Python script
 SCRIPT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
+# Default name for the configuration file
 CONFIG_DEFAULT_NAME = 'configurations.txt'
+
+# Indentation level for JSON output
+JSON_INDENT=2
 
 
 #----------------------------- ERROR MESSAGES ------------------------------#
+
+def message(text: str) -> None:
+    """Displays and logs a regular message to the standard error stream.
+    """
+    print(f"  {GREEN}>{DEFAULT_COLOR} {text}", file=sys.stderr)
+
 
 def warning(message: str) -> None:
     """Displays and logs a warning message to the standard error stream.
@@ -69,11 +81,11 @@ def fatal_error(error_message: str, *info_messages: str) -> None:
     """
     error(error_message)
     for info_message in info_messages:
-        print(f" {CYAN}\xF0\x9F\x9B\x88  {info_message}{DEFAULT_COLOR}", file=sys.stderr)
+        print(f" {CYAN}\u24d8  {info_message}{DEFAULT_COLOR}", file=sys.stderr)
     exit(1)
 
 
-#--------------------------- LOAD CONFIGURATION ----------------------------#
+#-------------------------- LOADING CONFIGURATION --------------------------#
 
 def read_filename(line: str) -> str:
     """Reads a filename from a line of text.
@@ -133,20 +145,20 @@ def extract_text(filepath: str, start_delimiter: str) -> str:
     return extracted_text
 
 
-def load_configurations(config_path):
+def load_configs(config_path):
     """Loads configurations from a file.
 
     Args:
-        config_path: The path to the configuration file.
+        config_path: The path to the config file.
     Returns:
         A tuple containing a dictionary of configurations by filename
         and a dictionary of global variables.
     """
-    configurations_by_filename = {}
-    configuration = {}
-    global_vars   = {}
-    filename      = ''
-    config_dir = os.path.dirname(config_path)
+    configs_by_filename = {}
+    config      = {}
+    global_vars = {}
+    filename    = ''
+    config_dir  = os.path.dirname(config_path)
 
     with open(config_path, 'r') as f:
         for line in f:
@@ -162,63 +174,91 @@ def load_configurations(config_path):
                 global_vars[key] = value
 
             elif line.startswith("./"):
-                # it's a filename, start of a new configuration
-                if filename and configuration:
-                    configurations_by_filename[filename] = configuration
+                # it's a filename, start of a new config
+                if filename and config:
+                    configs_by_filename[filename] = config
                 filename      = read_filename(line)
-                configuration = {}
+                config = {}
 
             else:
-                # it's a configuration variable
+                # it's a config variable
                 key, value = read_keyvalue(line)
                 if value.startswith("@"):
                     path_ = os.path.join(config_dir, value[1:])
                     value = extract_text(path_, f"./{filename}").strip()
-                configuration[key] = value
+                config[key] = value
                 continue
 
-    # if any configuration is pending, add it to the dictionary
-    if filename and configuration:
-        configurations_by_filename[filename] = configuration
+    # if any config is pending, add it to the dictionary
+    if filename and config:
+        configs_by_filename[filename] = config
 
-    return configurations_by_filename, global_vars
-
-
-#---------------------------------------------------------------------------#
-
-def apply_configuration_to_json(json, configuration, global_vars):
-    # dummy function
-    return json
+    return configs_by_filename, global_vars
 
 
-def make(filename: str, configuration: dict, global_vars: dict):
-    filename = filename if os.path.splitext(filename)[1] else filename + '.json'
-    print("-----------------------------------------")
-    print("filename:"     , filename      )
-    print("Configuration:", configuration )
-    print("Global vars:"  , global_vars   )
+#---------------------------- CREATING WORKFLOW ----------------------------#
+
+def create_workflow(template, config, global_vars):
+    # create a copy of the workflow dictionary to avoid modifying the original
+    workflow = template.copy()
+    return workflow
 
 
-def process(target: str, configurations_by_filename: dict, global_vars: dict):
+def make(filename: str, config: dict, global_vars: dict):
+    """Generates a JSON workflow file based on a template and configuration.
+
+    Args:
+        filename     (str): The name of the output JSON file.
+        config      (dict): A dict containing configuration values to be applied to the template.
+        global_vars (dict): A dict containing global variables used in the template.
+    """
+    workflow_filename = filename if os.path.splitext(filename)[1] else filename + '.json'
+    template_filename = global_vars.get('TEMPLATE')
+
+    if not template_filename:
+        fatal_error("No template is defined in the configuration.",
+                    "You must define the global variable @TEMPLATE in the configuration.")
+    if not os.path.isfile(template_filename):
+        fatal_error(f"The template file '{template_filename}' does not exist.",
+                    "Check the value assigned to the @TEMPLATE variable.")
+
+    # load the JSON template from the file
+    with open(template_filename, "r") as f:
+        template = json.load(f)
+
+    message(f"Building '{workflow_filename}'")
+    workflow = create_workflow(template, config, global_vars)
+
+    # Save the JSON workflow to a file
+    with open(workflow_filename, "w") as f:
+        json.dump(workflow, f,
+                  indent=(JSON_INDENT if JSON_INDENT>0 else None),
+                  ensure_ascii=False
+                  )
+
+
+def process(target: str, configs_by_filename: dict, global_vars: dict):
     """Processes a target based on provided configurations and global variables.
 
     Args:
-        target (str): The target to process. Can be 'clean', 'all', or a specific filename.
-        configurations_by_filename (dict): A dictionary mapping filenames to their corresponding configurations.
-        global_vars (dict): A dictionary containing global variables used during processing.
+        target              (str) : The target to process. Can be 'clean', 'all', or a specific filename.
+        configs_by_filename (dict): A dictionary mapping filenames to their corresponding configuration.
+        global_vars         (dict): A dictionary containing global variables used during processing.
     """
-    if target=='clean':
-        for filename, _ in configurations_by_filename.items():
-            print(f"rm {filename}")
+    if target == 'clean':
+        for filename, _ in configs_by_filename.items():
+            if os.path.isfile(filename):
+                message(f"Removing '{filename}'")
+                os.remove(filename)
 
-    elif target=='all':
-        for filename, configuration in configurations_by_filename.items():
-            make(filename, configuration, global_vars)
+    elif target == 'all':
+        for filename, config in configs_by_filename.items():
+            make(filename, config, global_vars)
 
     else:
-        filename      = target
-        configuration = configurations_by_filename.get(filename)
-        make(filename, configuration, global_vars)
+        filename = target
+        config   = configs_by_filename.get(filename)
+        make(filename, config, global_vars)
 
 
 #===========================================================================#
@@ -226,23 +266,31 @@ def process(target: str, configurations_by_filename: dict, global_vars: dict):
 #===========================================================================#
 
 def main():
+    global JSON_INDENT
     parser = argparse.ArgumentParser(
-        description="Create workflow files based on the configuration."
+        description="Create workflow files based on the config."
     )
     parser.add_argument('-c', '--config', metavar='FILE',
         help="Read FILE as the configurations (default: configurations.txt)"
+    )
+    parser.add_argument('-i', '--indent', type=int,
+        help=f"Indentation level for the generated JSON files (default: {JSON_INDENT})"
     )
     parser.add_argument('-v', '--version', action='version', version='wmake 0.2',
         help="Show version information and exit."
     )
     parser.add_argument('target', nargs='*', default=['all'],
         help="The name(s) of the workflow file(s) to create. "
-             "'all' to create all workflow files specified in the configuration. "
-             "'clean' to remove all workflow files specified in the configuration."
+             "'all' to create all workflow files specified in the config. "
+             "'clean' to remove all workflow files specified in the config."
     )
     args = parser.parse_args()
 
-    # determine the configuration file path
+    # set JSON indentation
+    if args.indent:
+        JSON_INDENT=args.indent
+
+    # determine the config file path
     if args.config:
         config_path = args.config
     elif os.path.exists(CONFIG_DEFAULT_NAME):
@@ -251,17 +299,17 @@ def main():
         config_path = os.path.join(SCRIPT_DIRECTORY, CONFIG_DEFAULT_NAME)
 
 
-    # check if the configuration file exists
+    # check if the config file exists
     if not os.path.exists(config_path):
-        fatal_error(f"Configuration file not found: {config_path}",
-                    "Please provide a valid configuration file path or ensure the default configuration file exists.")
+        fatal_error(f"config file not found: {config_path}",
+                    "Please provide a valid config file path or ensure the default config file exists.")
 
     # load the configurations
-    configurations_by_filename, global_vars = load_configurations(config_path)
+    configs_by_filename, global_vars = load_configs(config_path)
 
      # process each target
     for target in args.target:
-        process(target, configurations_by_filename, global_vars)
+        process(target, configs_by_filename, global_vars)
 
 
 if __name__ == "__main__":
